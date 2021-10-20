39090b85515035834a552b3880ed80204ad83dc0
EldosHD <Valentin.Reichenbach@live.com>
2021-10-20 06:55:59 +0200

    added an cmdline interface for the updated. the actual updater is still missing

--- jsonUpdater/jsonUpdater.py
+++ jsonUpdater/jsonUpdater.py
@@ -1,20 +1,86 @@
 import json
 import argparse
+import pathlib
+import logging as log
+
+log.info('test')
 
 hotKeys = [] # init array for the jsonfile
 
+hotKeyName = ''
+hotKeyType = ''
+hotKeyContent = ''
+
+givenJsonFile = 'hotKeys.json'
+
+
+parser = argparse.ArgumentParser(description='Not working yet')
+#options that are always available
+parentParser = argparse.ArgumentParser('The Parrent parser', add_help=False)
+parentParser.add_argument('--file', help='Not implemented yet',type=pathlib.Path)
+parentParser.add_argument('--no-color', help='Not implemented yet', default=True, action='store_false')
+parentParser.add_argument('--version', help='prints out the programs version number',action='version' ,version='%(prog)s 0.1')
+
+subparsers = parser.add_subparsers(description='valid subcommands NOTE: only use one of these', dest='subCommand')
+subparsers.required = True
+
+#create
+create = subparsers.add_parser('create',aliases=['cr'] , parents=[parentParser], help='creates a Hotkey (Note: If you provide a label that already exists it will override that hotkey)',description='NOTE: add description')
+
+create.add_argument('name', help='Not implemented yet')
+create.add_argument('type', help='Not implemented yet')
+create.add_argument('content', help='Not implemented yet')
+create.add_argument('-o','--output', help='creates the given file and dumps the updated input file in there')
+
+#merge
+merge = subparsers.add_parser('merge', aliases=['mg'], parents=[parentParser], help='merges the current hotkey file with a given one')
+merge.add_argument('merge', type=argparse.FileType('r'), help='Not implemented yet')
+
+#fix
+fix = subparsers.add_parser('fix', help='COMING SOON',parents=[parentParser])
+fix.add_argument('fix', help='Not implemented yet', action='store_true')
+fix.add_argument('--ignore-names', help='not implemented yet', action='store_true')
+
+#clean
+clean = subparsers.add_parser('clean', parents=[parentParser],aliases=['cl'] , help='cleans the given json file of unnessesary/garbage code (like e.g. a function with name "aq" or lable "420")')
+clean.add_argument('clean', help='Not implemented yet', action='store_true')
+
+
+
+args = parser.parse_args()
+
+print(args)
+
+
+def fix():
+    print('COMING SOON')
+
+def merge():
+    print('not implemented yet')
+
+def clean():
+    print('not implemented yet')
+
+def create():
+    print('create a hotkey')
+
 def main():
+    if args.file.name != 'hotKeys.json':
+        givenJsonFile = args.file.name
+    global hotKeys
     try:
-        with open('testFile.json') as jsonFile:
+        with open(givenJsonFile) as jsonFile:
             hotKeys = json.load(jsonFile)
         jsonFile.close()
         print('finished loading json file!')
     except BaseException as err:
-        print('\nERROR: Json File failed to load. The following exception was raised:')
-        print('\n' + str(err) + '\n')
+        print(f'\nThe given jsonFile ({givenJsonFile}) was not loaded correctly. By default this program looks for "hotKeys.json" in the current directory. You can specify anothe with --file [FILENAME]')
 
     print(f'Json file: {str(hotKeys)}')
 
+
+if __name__ == '__main__':
+    main()
 
 
 
@@ -25,16 +91,16 @@ def main():
     #create with given argument
 
 
-#list of arguments:
-#required:      the program shoudlnt start when its called without args, so it doesnt get called accidentilly
-    #name ==> creates label automaticly
-    #type
-    #required content for type
+#arguments to implement
+    #create
+    #merge
+    #fix
+    #clean
+    #no-color
+    #quiet
+    #verbosity
+    #aliases
+    #ignore names: doesnt fix the name of the hotkey(e.g. if sb added a hotkey for key "q" but named it "minecraft" --> wont change name to "q")
+    
 
-#optional:
-    #--no-color
-    #--merge: merges a given json file with the current one?
-    #--fix: repairs the current hotkey file
-    #--clean: removes unnessesary garbage code (like e.g. a function with name "aq" or lable "420")
-    #--file: uses a given json file instead of hotKeys.json (e.g. for --fix)
-
+# check if custom type applied --> no content needed
